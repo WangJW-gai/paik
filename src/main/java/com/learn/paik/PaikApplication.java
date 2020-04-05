a2d1bb210076764c8f3405704bd872e25e1adc13
Wangjw_gai <wangjw_gai@163.com>
2020-04-06 00:03:06 +0800

    搭建SpringBoot框架

---
+++ src/main/java/com/learn/paik/PaikApplication.java
@@ -0,0 +1,13 @@
+package com.learn.paik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/paik/PaikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.paik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
